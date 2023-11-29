d1e4611b06e25effb86243d61462bac969eb44aa
Saumya8 <63854202+Saumya8@users.noreply.github.com>
2023-11-29 17:14:24 +0000

    Game Class

--- qa-activity/GameManager.java
+++ qa-activity/GameManager.java
@@ -1,3 +1,56 @@
+import java.util.Collections;
+
+/**
+ * A utility class to handle the game logic for the Dominion game.
+ * This includes shuffling decks, drawing cards, and checking game end conditions.
+ */
 public class GameManager {
-    
+
+    /**
+     * Shuffles the specified deck.
+     *
+     * @param deck The deck to shuffle.
+     */
+    public static void shuffleDeck(Deck deck) {
+        Collections.shuffle(deck.getDrawPile());
+    }
+
+    /**
+     * Draws a specified number of cards from a player's deck.
+     *
+     * @param player The player drawing the cards.
+     * @param count The number of cards to draw.
+     */
+    public static void drawCards(Player player, int count) {
+        for (int i = 0; i < count; i++) {
+            player.getDeck().drawCard();
+        }
+    }
+
+    /**
+     * Checks if the game should end based on the Dominion game rules.
+     * The game ends when the stack of the most valuable victory card (Province cards) is depleted,
+     * or when any three stacks of cards in the supply are depleted.
+     *
+     * @param game The game to check the end condition for.
+     * @return True if the game should end, false otherwise.
+     */
+    public static boolean checkGameEndCondition(Game game) {
+        // Implement game end condition logic here
+        return false;
+    }
+
+    /**
+     * Calculates the total victory points for a player.
+     * This method sums up the victory points from all victory cards in a player's deck.
+     *
+     * @param player The player whose victory points are to be calculated.
+     * @return The total number of victory points.
+     */
+    public static int calculateVictoryPoints(Player player) {
+        // Implement victory points calculation here
+        return 0;
+    }
+
+    // Additional utility methods can be added as needed
 }
