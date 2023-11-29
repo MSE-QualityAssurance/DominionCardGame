b4f7ce4fd50b4bb743110be3f8e6ee8169fe31bf
Saumya8 <63854202+Saumya8@users.noreply.github.com>
2023-11-29 17:16:37 +0000

    UI, GameManager, Main Class

--- qa-activity/Main.java
+++ qa-activity/Main.java
@@ -1,3 +1,25 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        // Create an instance of the game
+        Game game = new Game();
+
+        // Initialize the UI
+        UI ui = new UI();
+
+        // Add players to the game
+        // This can be based on user input or a fixed number for demonstration
+        String playerName = ui.askQuestion("Enter player name:");
+        game.addPlayer(new Player(playerName));
+
+        // Additional player setup can be done here (e.g., adding more players)
+
+        // Initialize the game with necessary setup
+        // This includes setting up supply piles and players' decks
+        game.setupGame();
+
+        // Start the game
+        game.startGame();
+
+        // Game loop and logic will be handled within the Game class
+    }
 }
--- qa-activity/UI.java
+++ qa-activity/UI.java
@@ -1,3 +1,56 @@
+import java.util.Scanner;
+
+/**
+ * Handles all text-based user interactions for the Dominion game.
+ * This includes displaying game state and receiving player input.
+ */
 public class UI {
-    
+    private Scanner scanner;
+
+    /**
+     * Constructor for the UI class.
+     */
+    public UI() {
+        scanner = new Scanner(System.in);
+    }
+
+    /**
+     * Displays a message to the user.
+     *
+     * @param message The message to be displayed.
+     */
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    /**
+     * Asks a question to the user and returns the response.
+     *
+     * @param question The question to ask the user.
+     * @return The user's response as a string.
+     */
+    public String askQuestion(String question) {
+        System.out.println(question);
+        return scanner.nextLine();
+    }
+
+    /**
+     * Asks the user to choose an option from a list of choices.
+     *
+     * @param prompt   The prompt to display before the choices.
+     * @param choices  An array of choices for the user to select from.
+     * @return The index of the chosen option.
+     */
+    public int chooseOption(String prompt, String[] choices) {
+        System.out.println(prompt);
+        for (int i = 0; i < choices.length; i++) {
+            System.out.println((i + 1) + ". " + choices[i]);
+        }
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Clear the newline character from the input stream
+        return choice - 1; // Adjust for 0-based index
+    }
+
+    // Additional UI methods can be added as needed
 }
